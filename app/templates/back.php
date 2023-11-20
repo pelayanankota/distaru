4366de1cd6908d504457d21df601059e5b19f57b
fathur-abdul <foong.wob.5@gmail.com>
2023-11-20 15:10:43 +0700

    update dashboard

---
+++ app/templates/back.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dinas Cipta Karya, Bina Kontruksi dan Tata Ruang</title>
+    <link rel="icon" href="assets/dist/img/discipta.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap4/css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+    <style>
+        body {
+            background-image: url('background.jpg'); /* Ganti 'gambar-latar.jpg' dengan nama file gambar latar yang Anda inginkan */
+  background-size: cover; /* Untuk menutupi seluruh area background */
+  background-attachment: fixed; /* Membuat background tetap saat menggulir */
+  background-repeat: no-repeat; /* Tidak mengulang gambar background */
+  background-position: center center; /* Posisi gambar latar di tengah-tengah halaman */
+
+        }
+    </style>
+</head>
+<body>
+    <!-- Your website content goes here -->
+</body>
+</html>
